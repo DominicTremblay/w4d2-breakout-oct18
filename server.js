040afea299d58ffe4c00724f306b6565f751b88d
Dominic Tremblay <dominictremblay24@gmail.com>
2018-10-16 14:18:19 -0400

    created the knex query

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const config = require("./knexfile");
 const env = "development";
 const knex = require("knex")(config[env]);
+
+// Let's get the list of players that played the game 'card'
+
+knex
+  .select("first_name", "last_name", "played_on")
+  .from("players")
+  .innerJoin("game_details", "game_details.player_id", "players.id")
+  .innerJoin("games", "game_details.game_id", "games.id")
+  .where("games.name", "card")
+  .then(function(result) {
+    console.log(result);
+  })
+  .catch(error => {
+    console.log("error: ", error);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
